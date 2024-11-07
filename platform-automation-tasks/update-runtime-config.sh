42395ead1b1e6f4164198e9e7476b7b2556a9d04
Knappek <andy.knapp.ak@gmail.com>
2024-11-07 10:15:05 +0100

    add platform-automation-tasks in git repo

---
+++ platform-automation-tasks/update-runtime-config.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+# code_snippet update-runtime-config-script start bash
+
+cat /var/version && echo ""
+set -eu
+
+if [ -z "${NAME}" ]; then
+  { printf "\nError: 'NAME' parameter is required"; } 2>/dev/null
+  exit 1
+fi
+
+# shellcheck source=./setup-bosh-env.sh
+source ./platform-automation-tasks/tasks/setup-bosh-env.sh
+set -x
+
+if [ -e "releases/" ]; then
+  # $RELEASES_GLOB needs to be globbed to pass through properly
+  # shellcheck disable=SC2086
+  release_files="$(find releases/${RELEASES_GLOB} 2>/dev/null)"
+  if [ -n "${release_files}" ]; then
+    for rf in ${release_files}; do
+      bosh upload-release "${rf}"
+    done
+  fi
+fi
+
+vars_files_args=("")
+for vf in ${VARS_FILES}; do
+  vars_files_args+=("--vars-file ${vf}")
+done
+
+vars_env_args=("")
+if [[ -n "${BOSH_ENV_PREFIX}" ]]; then
+  vars_env_args+=("--vars-env ${BOSH_ENV_PREFIX}")
+fi
+
+# ${vars_files_args[@] needs to be globbed to pass through properly
+# shellcheck disable=SC2068
+bosh -n update-config \
+  --type runtime \
+  --name "${NAME}" \
+  config/"${CONFIG_FILE}" \
+  ${vars_files_args[@]} \
+  ${vars_env_args[@]}
+
+# code_snippet update-runtime-config-script end
