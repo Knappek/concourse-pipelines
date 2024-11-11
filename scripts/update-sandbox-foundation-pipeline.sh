a40e813ff832e622992d303ee67a971a526415b0
Knappek <andy.knapp.ak@gmail.com>
2024-11-11 09:20:48 +0100

    some updates and use small footprint tas

--- scripts/update-sandbox-foundation-pipeline.sh
+++ scripts/update-sandbox-foundation-pipeline.sh
@@ -4,9 +4,7 @@ set -eu
 
 echo "Setting sandbox foundation pipeline..."
 
-# Adds a tag to every job to tell the pipeline what remote worker to run on
-# for this pipeline, the remote worker is named 'vsphere-pez' because it is in the same environment as our vSphere
-fly -t platform-automation sp -p reference-pipeline \
-  -c  ./pipelines/pipeline.yml \
+fly -t ci sp -p deploy-sandbox-foundation \
+  -c  ./pipelines/deploy-foundation.yml \
   --var foundation=sandbox \
   --check-creds
