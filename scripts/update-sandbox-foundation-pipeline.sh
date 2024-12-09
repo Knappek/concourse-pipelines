0de4d54d04141ba05ce7ab434a39d8b859f419d7
Knappek <andy.knapp.ak@gmail.com>
2024-12-09 16:56:42 +0100

    fix update pipeline script

--- scripts/update-sandbox-foundation-pipeline.sh
+++ scripts/update-sandbox-foundation-pipeline.sh
@@ -7,5 +7,5 @@ echo "Setting sandbox foundation pipeline..."
 fly -t ci sp -p deploy-sandbox-foundation \
   -c  ./pipelines/deploy-foundation.yml \
   --var foundation=sandbox \
-  --load-vars-from=foundations/sandbox/var/tas.yml
+  --load-vars-from=./foundations/sandbox/vars/tas.yml \
   --check-creds
