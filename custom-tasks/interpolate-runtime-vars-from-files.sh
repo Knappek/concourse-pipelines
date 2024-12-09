cc7d60c62f92613699a415d4056e988c2cf00705
Knappek <andy.knapp.ak@gmail.com>
2024-12-09 16:55:12 +0100

    create custom task to generate gorouter ssl certs

---
+++ custom-tasks/interpolate-runtime-vars-from-files.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+cat /var/version && echo ""
+set -eux
+
+for vfm in ${VAR_FILEPATH_MAPPING}; do
+  key=$(echo "${vfm}" | cut -d":" -f1)
+  filepath=$(echo "${vfm}" | cut -d":" -f2)
+  flags+=("--var")
+  flags+=("${key}=$(cat files/${filepath})")
+done
+
+
+mkdir -p interpolated-files
+# ${flags[@] needs to be globbed to pass through properly
+# shellcheck disable=SC2068
+om interpolate --config "config/${CONFIG_PATH}" "${flags[@]}" > "interpolated-files/runtime-vars.yml"
--- scripts/update-sandbox-foundation-pipeline.sh
+++ scripts/update-sandbox-foundation-pipeline.sh
@@ -7,4 +7,5 @@ echo "Setting sandbox foundation pipeline..."
 fly -t ci sp -p deploy-sandbox-foundation \
   -c  ./pipelines/deploy-foundation.yml \
   --var foundation=sandbox \
+  --load-vars-from=foundations/sandbox/var/tas.yml
   --check-creds
