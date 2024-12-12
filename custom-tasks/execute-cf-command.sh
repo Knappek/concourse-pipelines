ac85e9174f6b08270caa694db5eb42faa352f169
Knappek <andy.knapp.ak@gmail.com>
2024-12-12 14:06:21 +0100

    specify cf_org and space

--- custom-tasks/execute-cf-command.sh
+++ custom-tasks/execute-cf-command.sh
@@ -13,5 +13,6 @@ if [ -n "${CF_ENVIRONMENT_VARIABLES_FILE}" ]; then
   source "${CF_ENVIRONMENT_VARIABLES_FILE}"
 fi
 
-cf login -a "${CF_API_URL}" "${login_options[@]}"
+cf login -a "${CF_API_URL}" "${login_options[@]}" -s "${CF_SPACE}"
+cf target -o "${CF_ORG}"
 cf "${CF_COMMAND}"
