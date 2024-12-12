0638ba3b648251851527741f52742dc8a4fc85e1
Knappek <andy.knapp.ak@gmail.com>
2024-12-12 14:46:44 +0100

    use sh for cf image

--- custom-tasks/execute-cf-command.sh
+++ custom-tasks/execute-cf-command.sh
@@ -1,18 +1,19 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 cat /var/version && echo ""
 set -eux
 
-login_options=""
-for cfl in ${CF_LOGIN_OPTIONS}; do
-  login_options+=("${cfl})")
-done
+# login_options=""
+# for cfl in ${CF_LOGIN_OPTIONS}; do
+#   login_options+=("${cfl})")
+# done
 
 if [ -n "${CF_ENVIRONMENT_VARIABLES_FILE}" ]; then
   # shellcheck disable=SC1090
-  source "${CF_ENVIRONMENT_VARIABLES_FILE}"
+  # source "${CF_ENVIRONMENT_VARIABLES_FILE}"
+  . "${CF_ENVIRONMENT_VARIABLES_FILE}"
 fi
 
-cf login -a "${CF_API_URL}" "${login_options[@]}" -s "${CF_SPACE}"
+cf login -a "${CF_API_URL}" "${CF_LOGIN_OPTIONS}" -s "${CF_SPACE}"
 cf target -o "${CF_ORG}"
 cf "${CF_COMMAND}"
