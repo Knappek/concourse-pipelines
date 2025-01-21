6ef00ee5cd45e783dbbae8ba4954435f41760073
Knappek <andy.knapp.ak@gmail.com>
2025-01-21 13:51:16 +0100

    create bank iso segment

--- scripts/update-sandbox-foundation-pipeline.sh
+++ scripts/update-sandbox-foundation-pipeline.sh
@@ -9,4 +9,5 @@ fly -t ci sp -p deploy-sandbox-foundation \
   --var foundation=sandbox \
   --load-vars-from=./foundations/sandbox/vars/tas.yml \
   --load-vars-from=./foundations/sandbox/vars/isolation-segment.yml \
+  --load-vars-from=./foundations/sandbox/vars/isolation-segment-bank.yml \
   --check-creds
